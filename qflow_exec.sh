a1382a2b1f558fba392bee7a932028dcc73eb881
gabrielchin96 <gabrielchin_2233@hotmail.com>
2022-07-13 22:47:59 +0800

    adding qflow tool and directories

---
+++ qflow_exec.sh
@@ -0,0 +1,17 @@
+#!/bin/tcsh -f
+#-------------------------------------------
+# qflow exec script for project /data
+#-------------------------------------------
+
+ /usr/local/share/qflow/scripts/yosys.sh /data ibex_top /data/source/ibex_top.sv || exit 1
+ /usr/local/share/qflow/scripts/graywolf.sh -d /data ibex_top || exit 1
+ /usr/local/share/qflow/scripts/vesta.sh  /data ibex_top || exit 1
+ /usr/local/share/qflow/scripts/qrouter.sh /data ibex_top || exit 1
+ /usr/local/share/qflow/scripts/vesta.sh  -d /data ibex_top || exit 1
+ /usr/local/share/qflow/scripts/magic_db.sh /data ibex_top || exit 1
+ /usr/local/share/qflow/scripts/magic_drc.sh /data ibex_top || exit 1
+ /usr/local/share/qflow/scripts/netgen_lvs.sh /data ibex_top || exit 1
+ /usr/local/share/qflow/scripts/magic_gds.sh /data ibex_top || exit 1
+# /usr/local/share/qflow/scripts/cleanup.sh /data ibex_top || exit 1
+# /usr/local/share/qflow/scripts/cleanup.sh -p /data ibex_top || exit 1
+# /usr/local/share/qflow/scripts/magic_view.sh /data ibex_top || exit 1
---
+++ qflow_vars.sh
@@ -0,0 +1,17 @@
+#!/bin/tcsh -f
+#-------------------------------------------
+# qflow variables for project /data
+#-------------------------------------------
+
+set qflowversion=1.4.80
+set projectpath=/data
+set techdir=/usr/local/share/qflow/tech/osu035
+set sourcedir=/data/source
+set synthdir=/data/synthesis
+set layoutdir=/data/layout
+set techname=osu035
+set scriptdir=/usr/local/share/qflow/scripts
+set bindir=/usr/local/share/qflow/bin
+set logdir=/data/log
+#-------------------------------------------
+
